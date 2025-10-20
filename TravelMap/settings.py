ba82d5e3067f8f33d128ecdf53c8406f7ef14b4c
Lakshya Dubey <Lakshya_717@outlook.com>
2025-10-20 14:54:29 +0530

    changed timezone

--- TravelMap/settings.py
+++ TravelMap/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'TravelMap.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
